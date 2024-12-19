cc511485b6b1d10bdef90b09132044a2626cd0c6
Darío Martínez Kostyuk <dariomartk@gmail.com>
2024-12-19 14:02:22 +0100

    feat: Tests of the crud Destinos

---
+++ storage/framework/views/a534cc1dee811f63f53182b0a9046c68.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SprintCode</title>
+    <link rel="icon" href="https://i.ibb.co/gWWr6tN/image-removebg-preview.png" type="image/x-icon">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link href="../css/app.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+<?php if (isset($component)) { $__componentOriginala591787d01fe92c5706972626cdf7231 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginala591787d01fe92c5706972626cdf7231 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.navbar','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('navbar'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?> <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginala591787d01fe92c5706972626cdf7231)): ?>
+<?php $attributes = $__attributesOriginala591787d01fe92c5706972626cdf7231; ?>
+<?php unset($__attributesOriginala591787d01fe92c5706972626cdf7231); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginala591787d01fe92c5706972626cdf7231)): ?>
+<?php $component = $__componentOriginala591787d01fe92c5706972626cdf7231; ?>
+<?php unset($__componentOriginala591787d01fe92c5706972626cdf7231); ?>
+<?php endif; ?>
+    <section class="content container-fluid ">
+        <div class="row justify-content-center align-items-center d-flex vh-50">
+            <div class="col-md-7">
+
+                <div class="card card-default">
+                    <div class="card-header">
+                        <span class="card-title"><?php echo e(__('Crear')); ?> Asignatura</span>
+                    </div>
+                    <div class="card-body bg-white">
+                        <form method="POST" action="<?php echo e(route('asignaturas.store')); ?>"  role="form" enctype="multipart/form-data">
+                            <?php echo csrf_field(); ?>
+
+                            <?php echo $__env->make('asignatura.form', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</body>
+</html>
+<?php /**PATH /home/dario/universidad/is/sprintcode/resources/views/asignatura/create.blade.php ENDPATH**/ ?>
---
+++ tests/Feature/DestinosTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Destino;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DestinosTest extends TestCase
+{
+    public function test_create(): void
+    {
+        //Data to insert on the database destinos
+        $datos1 = [
+            'nombreCiudad' => 'Test_1',
+            'nombreUniversidad' => 'Universidad de Test_1',
+            'especialidad' => 'informatica'
+        ];
+        //Save the data on the database
+        $create1 = $this->post('/storeDest', $datos1);
+        //Check if redirects to the main page
+        $create1->assertRedirect('/');
+        //Check if the data is on the database
+        $this->assertDatabaseHas('destinos', $datos1);
+
+        //Data to insert on the database destinos
+        $datos2 = [
+            'nombreCiudad' => 'Test_2',
+            'nombreUniversidad' => 'Universidad de Test_2',
+            'especialidad' => 'informatica'
+        ];
+        //Save the data on the database
+        $create2 = $this->post('/storeDest', $datos2);
+        //Check if redirects to the main page
+        $create2->assertRedirect('/');
+        //Check if the data is on the database
+        $this->assertDatabaseHas('destinos', $datos2);
+    }
+
+    public function test_modify()
+    {
+        //Data of the current destination to modify, we will modify the first that we created in the previous test
+        $datos1 = Destino::where('nombreCiudad', 'Test_1')->first();
+        //Data to modify the destination
+        $datos1->nombreCiudad = 'Test_1_Modified';
+        $datos1->nombreUniversidad = 'Universidad de Test_1_Modified';
+        //Save the data on the database
+        $modify1 = $this->put('/ModDest/' . $datos1->id, $datos1->toArray());
+        //Check if redirects to the main page
+        $modify1->assertRedirect('/');
+        //Check if the data modified is on the database
+        $this->assertDatabaseHas('destinos', ['id' => $datos1->id,
+            'nombreCiudad' => $datos1->nombreCiudad,
+            'nombreUniversidad' => $datos1->nombreUniversidad,
+            'especialidad' => $datos1->especialidad]);
+
+        //Data of the current destination to modify, we will modify the second that we created in the previous test
+        $datos2 = Destino::where('nombreCiudad', 'Test_2')->first();
+        //Data to modify the destination
+        $datos2->nombreCiudad = 'Test_2_Modified';
+        $datos2->nombreUniversidad = 'Universidad de Test_2_Modified';
+        //Save the data on the database
+        $modify2 = $this->put('/ModDest/' . $datos2->id, $datos2->toArray());
+        //Check if redirects to the main page
+        $modify2->assertRedirect('/');
+        //Check if the data modified is on the database
+        $this->assertDatabaseHas('destinos', ['id' => $datos2->id,
+            'nombreCiudad' => $datos2->nombreCiudad,
+            'nombreUniversidad' => $datos2->nombreUniversidad,
+            'especialidad' => $datos2->especialidad]);
+    }
+
+    public function test_delete()
+    {
+        //Select the first destination created in the previous tests
+        $datos1 = Destino::where('nombreCiudad', 'Test_1_Modified')->first();
+        //Delete the destination
+        $delete1 = $this->get('/delete/' . $datos1->id);
+        //Check if redirects to the main page
+        $delete1->assertRedirect('/');
+        //Check if the data is not on the database
+        $this->assertDatabaseMissing('destinos', ['id' => $datos1->id,
+            'nombreCiudad' => $datos1->nombreCiudad,
+            'nombreUniversidad' => $datos1->nombreUniversidad,
+            'especialidad' => $datos1->especialidad]);
+
+        //Select the second destination created in the previous tests
+        $datos2 = Destino::where('nombreCiudad', 'Test_2_Modified')->first();
+        //Delete the destination
+        $delete2 = $this->get('/delete/' . $datos2->id);
+        //Check if redirects to the main page
+        $delete2->assertRedirect('/');
+        //Check if the data is not on the database
+        $this->assertDatabaseMissing('destinos', ['id' => $datos2->id,
+            'nombreCiudad' => $datos2->nombreCiudad,
+            'nombreUniversidad' => $datos2->nombreUniversidad,
+            'especialidad' => $datos2->especialidad]);
+    }
+}
