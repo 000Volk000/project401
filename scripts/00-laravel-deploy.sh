6db66fc959d1b945c6ef92aae2f6617d37b4847d
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2024-12-20 11:10:54 +0100

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+composer install --no-dev --optimize-autoloader
+
+echo "Setting permissions"
+sudo chown -R www-data:www-data /var/www \
+    && chmod -R 775 /var/www/storage \
+    && chmod -R 775 /var/www/bootstrap/cache
 
 echo "Caching config..."
 php artisan config:cache
@@ -9,10 +14,13 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+echo "Caching view..."
+php artisan view:cache
+
 echo "Running migrations..."
 php artisan migrate --force
 
 echo "Running seeders"
-artisan db:seed --force
+php artisan db:seed --force
 
 echo "done deploying"
