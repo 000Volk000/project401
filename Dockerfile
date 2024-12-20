6dc2e84de0f69594ad295c0f25e4617002d1e9f9
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2024-12-20 14:07:07 +0100

    feat: dockerfile for production

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,15 @@
-# Use official PHP image
-FROM php:8.2-fpm
-
-# Set working directory
-WORKDIR /var/www/html
-
-# Copy application files
+FROM richarvey/nginx-php-fpm:1.7.2
 COPY . .
-
-# Install system dependencies
-RUN apt-get update && apt-get install -y \
-    libpq-dev \
-    && docker-php-ext-install pdo pdo_mysql
-
-# Install Composer
-COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
-
-# Install PHP dependencies
-RUN composer install --no-dev --optimize-autoloader
-
-# Set permissions
-RUN chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache \
-    && chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
-
-# Expose port
-EXPOSE 9000
-
-# Add start.sh script
-COPY start.sh /start.sh
-RUN chmod +x /start.sh
-
-# Start the application
+# Image config
+ENV SKIP_COMPOSER 1
+ENV WEBROOT /var/www/html/public
+ENV PHP_ERRORS_STDERR 1
+ENV RUN_SCRIPTS 1
+ENV REAL_IP_HEADER 1
+# Laravel config
+ENV APP_ENV production
+ENV APP_DEBUG false
+ENV LOG_CHANNEL stderr
+# Allow composer to run as root
+ENV COMPOSER_ALLOW_SUPERUSER 1
 CMD ["/start.sh"]
--- storage/framework/views/424baf4b7b6ba4fb8d8f0ae2d447072b.php
+++ storage/framework/views/424baf4b7b6ba4fb8d8f0ae2d447072b.php
@@ -16,9 +16,27 @@
 
 </head>
 <body>
-
+<?php if (isset($component)) { $__componentOriginala591787d01fe92c5706972626cdf7231 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginala591787d01fe92c5706972626cdf7231 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.navbar','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('navbar'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?> <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginala591787d01fe92c5706972626cdf7231)): ?>
+<?php $attributes = $__attributesOriginala591787d01fe92c5706972626cdf7231; ?>
+<?php unset($__attributesOriginala591787d01fe92c5706972626cdf7231); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginala591787d01fe92c5706972626cdf7231)): ?>
+<?php $component = $__componentOriginala591787d01fe92c5706972626cdf7231; ?>
+<?php unset($__componentOriginala591787d01fe92c5706972626cdf7231); ?>
+<?php endif; ?>
     <section class="content container-fluid">
-        <div class="row justify-content-center align-items-center d-flex vh-100">
+        <div class="row justify-content-center align-items-center d-flex vh-50">
             <div class="col-md-7">
 
                 <div class="card card-default">
